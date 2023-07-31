ec6b38af5670e1c7263de1eaded649462853ebfc
Bohdan <bohdan.ko@starkey.co.il>
2023-07-31 13:35:11 +0200

    Update: descrived widget states to be done

---
+++ src/components/base/WidgetIconButton.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "WidgetIconButton"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/composables/useWidgetState.ts
+++ src/composables/useWidgetState.ts
@@ -1,3 +1,22 @@
 import { ref } from 'vue'
 
+
+/**
+ * Possible states:
+ *
+ * 1. Not initialized - when initFunction was not called, should just show the warning message without any buttons
+ * 2. Initialized:
+ * ---- 1. Inactive - when there is an active instance of this widget on different tab. Should show button 'activate'
+ * ---- 2. Active - this widget instance is activated:
+ * -------- 1. Idle - when no call is active:
+ * ------------ 1. Target input - if making outgoing calls is allowed
+ * ------------ 2. Empty view - if making outgoing calls is disabled:
+ * ---------------- 1. Minimized empty view - if option 'shrinkOnIdle' is enabled
+ * ---------------- 2. Maximized empty view - if option 'shrinkOnIdle' is disabled
+ * -------- 2. Active call - when call is active:
+ * ------------ 1. One call - when there is only one call. Show the call and it's actions
+ * ------------ 2. Few calls/rooms
+ * -------- 3. Ringing - when there is an incoming call. Should show button 'answer' and 'reject'. If option 'autoAnswer' is enabled and there is no active call, should answer automatically, otherwise should show button 'answer' and 'reject'
+ */
+
 export const isSettingsPageOpened = ref<boolean>(false)
