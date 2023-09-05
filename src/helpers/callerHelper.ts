0374b3b7d185dd8693845b2a758f9b14f531e34f
Bohdan <bohdan.ko@starkey.co.il>
2023-09-05 10:11:37 +0200

    Fix: - masking caller number

--- src/helpers/callerHelper.ts
+++ src/helpers/callerHelper.ts
@@ -16,16 +16,24 @@ export function getCallerInfo (number: string, name: string = '', displayName: b
 }
 
 export function getCallerNumber (number: string, useMask: boolean = false) {
-    const onlyNumber = number[0] === '+' ? number.slice(1) : number
+    const hasPlus = number[0] === '+'
+    const onlyNumber = hasPlus ? number.slice(1) : number
 
-    if (useMask && onlyNumber.length === 12) {
-        const countryCode = onlyNumber.slice(0, 3)
-        const lastDigit = onlyNumber[onlyNumber.length - 1]
-        return `${countryCode}XXXXXXXX${lastDigit}`
-    } else if (useMask && onlyNumber.length === 10) {
-        const lastDigit = onlyNumber[onlyNumber.length - 1]
-        return `XXXXXXXXX${lastDigit}`
+    if (useMask) {
+        let countryCode = ''
+        let mainNumber = onlyNumber
+
+        if (onlyNumber.length > 10) {
+            countryCode = onlyNumber.slice(0, onlyNumber.length - 10)
+            mainNumber = onlyNumber.slice(onlyNumber.length - 10)
+        }
+
+        const lastDigit = mainNumber[mainNumber.length - 1]
+        const maskedMainNumber = 'X'.repeat(mainNumber.length - 1)
+
+        return `${countryCode}${maskedMainNumber}${lastDigit}`
     } else {
         return number
     }
 }
+
